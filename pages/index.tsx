baaa2e8428735af46a7749f8e902f4709e1ee525
joenapper <joe.napper@hotmail.co.uk>
2022-08-18 10:07:14 +0100

    revision: remove MeetTheTeam section from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import CallToAction from "../components/CallToAction";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import GetInTouch from "../slices/GetInTouch";
-import MeetTheTeam from "../slices/MeetTheTeam";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
-      <MeetTheTeam />
       <CallToAction
         title="Make the change"
         text="Take your first step to better health by filling out our free
