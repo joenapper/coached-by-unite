bc9bc4086a674da1cf2617412f6b72de165c177b
joenapper <joe.napper@hotmail.co.uk>
2022-08-22 14:06:38 +0100

    feat: create UniteMobileApp slice

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import CallToAction from "../components/CallToAction";
 import Hero from "../components/Hero";
 import Layout from "../components/Layout";
 import GetInTouch from "../slices/GetInTouch";
+import UniteMobileApp from "../slices/UniteMobileApp";
 
 export default function Home() {
   return (
     <Layout>
       <Hero />
+      <UniteMobileApp />
       <CallToAction
         title="Make the change"
         text="Take your first step to better health by filling out our free
---
+++ slices/UniteMobileApp/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ImageWrapper = styled.div`
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+export const SubSubHeading = styled.h3`
+  font-weight: 300;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
+
+export const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
