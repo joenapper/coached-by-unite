a38b91e66aaec37482c316e6c9f192f57a9e5f34
joenapper <joe.napper@hotmail.co.uk>
2023-08-02 21:08:38 +0100

    chore: remove our journey page and related code

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -47,7 +47,6 @@ const Header = ({ isHomepage }: HeaderProps) => {
               <a>Home</a>
             </Link>
           </li>
-          {/* Our Journey Link here */}
           <li className="mobile-only" onClick={() => setNavActive(false)}>
             <a href="#contact">Contact form</a>
           </li>
